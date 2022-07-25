7c9df5007827c513b48875fd6e22af770191a533
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-07-25 20:43:52 -0300

    Update exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -2,7 +2,7 @@
 function maior(n1,n2,n3,n4){
 return Math.max(n1,n1,n3,n4);
 }
-let maiorNum = maior(5,10,20,20)
 
-console.log(maiorNum + maiorNum);
+let maiorNum = maior(5,10,15,20)
 
+console.log(maiorNum += 15);
