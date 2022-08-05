f5ca97d96e666e6b93e5253999ddf1d362fa2223
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-08-04 22:37:26 -0300

    Create exercicio5.js

---
+++ exercicio5.js
@@ -0,0 +1,6 @@
+//Ler dois números inteiros e exibir o quociente e o resto da divisão inteira entre eles.
+var a = 15;
+var b = 5;
+var res = a%b;
+var quo = (a-res)/b;
+console.log("Quociente = ", quo ,"Restante = ", res);
