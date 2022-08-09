1f6badf5f2e1711275187c559743d4c72552f557
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-08-09 20:13:41 -0300

    Update exercicio5_calcular_media.js

--- exercicio5_calcular_media.js
+++ exercicio5_calcular_media.js
@@ -1,17 +1,11 @@
 //Leia 4 números e calcule a média
-var valor1 = 10;
-var valor2 = 10;
-var valor3 = 10;
-var valor4 = 10;
-var total = 0;
-var media = 0;
-
-total = valor1 + valor2 + valor3 + valor4;
-media = total / 4;
-console.log("Programa que calcula a media de 4 números")
-console.log("1 Valor: " + valor1);
-console.log("2 Valor: " + valor2);
-console.log("3 Valor: " + valor3);
-console.log("4 Valor: " + valor4);
-console.log("Total: " + total)
-console.log("Media: " + media);
+function ArrayAvg(myArray) {
+    var i = 0, summ = 0, ArrayLen = myArray.length;
+    while (i < ArrayLen) {
+        summ = summ + myArray[i++];
+}
+    return summ / ArrayLen;
+}
+var myArray = [1, 5, 2, 3];
+var a = ArrayAvg(myArray);
+console.log(a)
