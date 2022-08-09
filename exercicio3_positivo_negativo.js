7574904036f82d1560702301a7d3291d3a3478e0
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-08-09 14:55:13 -0300

    Update exercicio3_positivo_negativo.js

--- exercicio3_positivo_negativo.js
+++ exercicio3_positivo_negativo.js
@@ -1,13 +1,15 @@
 //Leia um número e informe se o mesmo é positivo, negativo ou zero.
-function algoritmo()
+function algoritmo(numero)
 {
     let um_numero;
-    um_numero = parseFloat (document.forma1.um_numero.value);
+    um_numero = parseFloat (numero);
     if(um_numero>0)
         alert('Positivo');
     if(um_numero<0)
         alert('Negativo');
-    if(um_numero==0)
-        alert('Neutro');
-    console.log("Nem positivo nem negativo, zero");
+    if(um_numero==0){
+       alert('Neutro');
+       console.log("Nem positivo nem negativo, zero");
+    }
+
 }
