55de4fcddc940e2ca47b4c9a2d3564ba6f67d5f3
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-08-10 18:29:41 -0300

    Update exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -2,6 +2,7 @@
 var num1 = 10;
 var num2 = 5; 
 var num3 = 2;
+var num4 = 6;
 
 var maior = num1;
 
@@ -9,6 +10,8 @@ if(num2 > maior){
       maior = num2;
  } if(num3 > maior){
       maior = num3;
- }
+ }if(num4 > maior){
+      maior = num4;
+       
      console.log("Maior: "+maior);
      
