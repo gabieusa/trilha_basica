f6f42bd75c4df086c8fd5384ac3ffc9f5d1c3cb8
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-09-18 21:16:24 -0300

    Update exercicio11.php

--- exercicio11.php
+++ exercicio11.php
@@ -1,16 +1,42 @@
 //Leia uma letra e mostre se ela é vogal ou consoante.
 <?php
-function vogalouconsoante($x)
-{
-    if ($x == 'a' || $x == 'e' ||
-        $x == 'i' || $x == 'o' ||
-        $x == 'u')
-        echo "vogal" . "\n";
-    else
-        echo "consoante" . "\n";
+function lerLetra() {
+    echo ("Introduza uma letra: ");
 }
- 
-vogalouconsoante('c');
-vogalouconsoante('e');
 
+function isVogal($caracter) {
+    switch ($caracter) {
+        case "a":
+        case "A":
+        case "e":
+        case "E":
+        case "i":
+        case "I":  
+        case "o":
+        case "O":
+        case "u":
+        case "U": 
+            echo true;
+        default:
+            echo false;
+    }
+}
+
+ $letra = lerLetra();
+if (isVogal($letra)) echo("A letra é vogal");
+else echo("A letra é consoante");
 ?>
+
+
+//function vogalouconsoante($x)
+//{
+ //   if ($x == 'a' || $x == 'e' ||
+ //       $x == 'i' || $x == 'o' ||
+ //       $x == 'u')
+        echo "vogal" . "\n";
+ //   else
+ //       echo "consoante" . "\n";
+//}
+ 
+//vogalouconsoante('c');
+//vogalouconsoante('e');
