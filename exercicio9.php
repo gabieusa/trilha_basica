f4882cad1a9847f52c879a07ff24b36aa95dd782
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-09-19 08:45:42 -0300

    Update exercicio9.php

--- exercicio9.php
+++ exercicio9.php
@@ -1,16 +1,14 @@
-//Leia três números e mostre o maior entre eles
 <?php 
+//Leia três números e mostre o maior entre eles
 $n1 = 10; 
 $n2 = 50; 
 $n3 = 7; 
 $ordem = array($n1,$n2,$n3); 
 sort($ordem); 
 foreach ($ordem as $l ) { 
-echo "$l "; 
+    echo "$l "; 
 } 
-$maior = ($n1 > $n2)? $n1: $n2; 
-$maior = ($maior > $n3)? $maior: $n3; 
-echo "<br>O maior numero é $maior"; 
+echo "<br>O maior numero é ".max($ordem); 
 //.$ordem[2]; 
 //.max($ordem); 
 ?>
