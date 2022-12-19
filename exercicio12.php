7379a77fa3490baf2be0bd7f3e54882ca4d5c6be
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-12-19 07:26:40 -0300

    Update exercicio12.php

--- exercicio12.php
+++ exercicio12.php
@@ -1,7 +1,7 @@
 <?php
 $ano;
-if (isset($_GET['ano'])) {
-$ano = $_GET['ano'];
+if (isset($_GET["ano"])) {
+$ano = $_GET["ano"];
 }
 if (($ano % 100 != 0 && $ano % 4 == 0) || ($ano % 400 == 0)) {
 echo "Esse ano é bissexto";
