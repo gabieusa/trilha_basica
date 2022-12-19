9c7643642edbce5dcc80a23dd615b226a99cc208
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-12-19 08:07:21 -0300

    Update exercicio12.php

--- exercicio12.php
+++ exercicio12.php
@@ -1,4 +1,8 @@
- <?php
+//Escreva um programa que verifique se um ano é bissexto. Um ano é bissexto se ele
+é divisível por 4. Entretanto, se o ano é divisível por 100, ele não é bissexto. Mas, se
+ele for divisível por 400, ele volta a ser bissexto.
+
+<?php
 function verificaAnoBissexto(int $ano)
             {
                 if (($ano % 4) == 0 && ($ano % 100) <> 0) {
