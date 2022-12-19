e5f3f5932fb05d759b42298b0e04e71eaf5d79d8
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-12-19 07:17:03 -0300

    Create exercicio12.php

---
+++ exercicio12.php
@@ -0,0 +1,10 @@
+$ano;
+if (isset($_GET['ano'])) {
+$ano = $_GET['ano'];
+}
+if (($ano % 100 != 0 && $ano % 4 == 0) || ($ano % 400 == 0)) {
+echo "Esse ano é bissexto";
+}
+else {
+echo "Esse ano não é bissexto";
+}
