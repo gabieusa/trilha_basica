05136ea9e73674500e222484a8ebe29990c25858
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-12-19 08:05:45 -0300

    Update exercicio12.php

--- exercicio12.php
+++ exercicio12.php
@@ -1,3 +1,22 @@
+ <?php
+function verificaAnoBissexto(int $ano)
+            {
+                if (($ano % 4) == 0 && ($ano % 100) <> 0) {
+                    echo "O ano de $year é BISSEXTO. <br>";
+                } elseif ($ano % 400 == 0) {
+                    echo  "O ano de $ano é BISSEXTO. <br>";
+                } else {
+                    echo "O ano de $ano não é BISSEXTO. <br>";
+                }
+               
+            }
+
+            $ano = $_GET["ano"] ?? 0;
+          
+  ?>
+
+
+
 <?php
 $ano;
 if (isset($_GET["ano"])) {
