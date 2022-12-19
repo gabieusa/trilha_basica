887c0b8e382901ce29444f47520ebf2c00019469
Gabriela <84246788+gabieusa@users.noreply.github.com>
2022-12-19 08:26:38 -0300

    Create exercicio24.php

---
+++ exercicio24.php
@@ -0,0 +1,14 @@
+//Escreva um programa que imprima todos os números de 1 até 100, inclusive, e a
+soma de todos eles.
+
+<?php
+
+$numero = 0;
+$resultado = [];
+
+for($i = ($numero+1);$i <= 100;$i++){
+$resultado[] += $i;
+}
+
+print_r($resultado);
+  ?>
